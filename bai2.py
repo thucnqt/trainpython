3975eeabdff1f29ce1abd34680dc35fd7abd370f
Tan Tran <2188362+tant@users.noreply.github.com>
2020-02-05 12:31:49 +0700

    Create bai2.py
    Thêm bài 2

---
+++ bai2.py
@@ -0,0 +1,18 @@
+# Viết một chương trình với mô tả sau
+# Khi chạy chương trình sẽ hiện ra như sau
+
+#  Xin chào! Hãy nhập tên đầy đủ bên dưới (gõ luôn cả dấu tiếng Việt nha)
+#  -
+
+# Người dùng sẽ nhập họ tên đầy đủ của mình sau đó chương trình sẽ làm các việc sau
+# Tính toán để lấy được họ, tên và chữ lót
+# Sau đó hiện ra câu
+#  Chúc bạn <tên đã nhận ra được> một ngày tốt lành!
+
+# Rồi lưu vào file danhsachten.csv thêm một line với format sau 
+#  chuỗi họ tên, chuỗi họ, chuỗi tên, chuỗi chữ lót [và xuống dòng]
+
+# ví dụ anh nhập vào Trần Xuân Ngọc Tân thì chương trình sẽ xuất câu "Chúc bạn Tân một ngày tốt lành"
+# đồng thời lưu thêm 1 dòng vào file có nội dung sau "Trần Xuân Ngọc Tân, Trần, Tân, Xuân Ngọc"
+
+# Thử suy nghĩ xem nếu user nhập lung tung vào thì họ sẽ nhập những gì và chương trình của mình sẽ đối phó như thế nào
