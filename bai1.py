a2d50d639c8e0a1129534fe44a6133be787d859b
Tan Tran <2188362+tant@users.noreply.github.com>
2020-02-05 14:52:41 +0700

    Update bai1.py
    cập nhật lại bài 1 cho cụ thể

--- bai1.py
+++ bai1.py
@@ -5,10 +5,10 @@ strprice = '$27.3'
 strtime0 = '2019-12-07T21:31:56.000Z'
 
 
-#khai báo biến fprice và gán cho nó giá trị bằng với strprice
+#khai báo biến kiểu số thực fprice và gán cho nó giá trị bằng với strprice
 fprice = <viết code của em ở đây>
 
-# khai báo biến dttime0 và gán cho nó giá trị của stretime0
+# khai báo biến kiểu datetime dttime0 và gán cho nó giá trị của stretime0
 dttime0 = <viết code của em ở đây> 
 
 # tạo biến dttime1 có giá trị bằng giá trị của dttime0 cộng thêm 1 giờ 2 phút 3 giây
