976d941d55fd6e6ec5ec9b555259315a859bbaad
Tan Tran <2188362+tant@users.noreply.github.com>
2020-02-05 12:09:58 +0700

    Create bai1.py
    thêm bài tập 1 để học chuyển đổi kiểu dữ liệu

---
+++ bai1.py
@@ -0,0 +1,18 @@
+import datetime
+
+strprice = '$27.3'
+
+strtime0 = '2019-12-07T21:31:56.000Z'
+
+
+#khai báo biến fprice và gán cho nó giá trị bằng với strprice
+fprice = <viết code của em ở đây>
+
+# khai báo biến dttime0 và gán cho nó giá trị của stretime0
+dttime0 = <viết code của em ở đây> 
+
+# tạo biến dttime1 có giá trị bằng giá trị của dttime0 cộng thêm 1 giờ 2 phút 3 giây
+dttime1 = <viết code của em ở đây> 
+
+# Xuất 3 giá trị đã tính ra màn hình
+<viết code của em ở đây>
